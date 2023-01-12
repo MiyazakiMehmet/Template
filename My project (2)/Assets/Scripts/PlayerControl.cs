e0124523c0313f06e3d695cceb22980127aa257a
Mehmet <mehmetkoray_@hotmail.com>
2023-01-12 14:48:09 +0300

    Shotgun added

--- My project (2)/Assets/Scripts/PlayerControl.cs
+++ My project (2)/Assets/Scripts/PlayerControl.cs
@@ -9,32 +9,46 @@ public class PlayerControl : MonoBehaviour
     public float dashSpeed;
     Vector3 mousePos;
     public Transform Firepoint;
+    public Transform ReferencePoint;
+    public GameObject particle;
 
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
+    //Bullet
+    public int bulletCount = 10;
+    public float bulletSpeed;
+    public float spread;
 
     void Update()
     {
-        if (Input.GetMouseButton(0))
+        if (Input.GetButtonDown("Fire1"))
         {
             GoDirection();
+            Shoot();
         }
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition) -ReferencePoint.position;
     }   
 
     void FixedUpdate()
     {
-        Vector3 targetDirection = mousePos - Firepoint.position;
-        float angle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
     void GoDirection()
     {
-        rb.AddForce(-Firepoint.up * dashSpeed, ForceMode2D.Impulse);
+        rb.AddForce(-Firepoint.right * dashSpeed, ForceMode2D.Impulse);    }
+
+    void Shoot()
+    {
+        for(int i = 0; i < bulletCount; i++)
+        {
+            GameObject bullet = Instantiate(particle, Firepoint.position, Firepoint.rotation);
+            Rigidbody2D BulletRB = bullet.GetComponent<Rigidbody2D>();
+            Vector2 dir = Firepoint.transform.rotation * Vector2.right;
+            Vector2 pdir = Vector2.Perpendicular(dir) * Random.Range(-spread, spread);
+            BulletRB.velocity = (dir + pdir) * bulletSpeed;
+            Destroy(bullet, 1f);
+        }
     }
 
 }
