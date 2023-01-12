d49b81c5c107a327725b3ff3ec8267dc280084dc
Mehmet <mehmetkoray_@hotmail.com>
2023-01-12 16:55:05 +0300

    Camera Vertical Shifting

--- My project (2)/Assets/Scripts/BG_Scroller.cs
+++ My project (2)/Assets/Scripts/BG_Scroller.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class BG_Scroller : MonoBehaviour
 {
+    [SerializeField] private PlayerControl playerControl;
 
-    [Range(-1f, 1f)] public float shiftSpeed;
-    private float offset;
-    private Material mat;  
+    [Range(-1f, 1f)] public float shiftSpeedX, shiftSpeedY;
+    private float offsetX, offsetY;
+    private Material mat;
+
+    //Restricts
+    public Transform maxPos;
+    public Transform minPos;
 
     // Start is called before the first frame update
     void Start()
@@ -18,7 +23,21 @@ public class BG_Scroller : MonoBehaviour
     // Update is called once per frame  
     void Update()
     {
-        offset += (Time.deltaTime * shiftSpeed) / 10f;
-        mat.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+        offsetX += (Time.deltaTime * shiftSpeedX) / 10f;
+        offsetY += (Time.deltaTime * shiftSpeedY) / 10f;
+        mat.SetTextureOffset("_MainTex", new Vector2(offsetX, offsetY));
+        
+        if(playerControl.transform.position.y >= maxPos.position.y)
+        {
+            shiftSpeedY = 1.2f;
+        }
+        else if(playerControl.transform.position.y <= minPos.position.y)
+        {
+            shiftSpeedY = -1.2f;
+        }
+        else
+        {
+            shiftSpeedY = 0f;
+        }
     }
 }
