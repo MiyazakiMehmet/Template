3fbca14755d9c1b392ec690a07a6748c41988678
Mehmet <mehmetkoray_@hotmail.com>
2023-01-12 14:55:38 +0300

    Adjusted

--- My project (2)/Assets/Scripts/PlayerControl.cs
+++ My project (2)/Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    public Camera cam;
+    public Camera cam
+        ;
+    //Rigidbody of Character
     public Rigidbody2D rb;
     public float dashSpeed;
+
+    //Positions
     Vector3 mousePos;
     public Transform Firepoint;
     public Transform ReferencePoint;
-    public GameObject particle;
 
     //Bullet
+    public GameObject bulletPrefab;
     public int bulletCount = 10;
     public float bulletSpeed;
     public float spread;
@@ -25,24 +29,29 @@ public class PlayerControl : MonoBehaviour
             Shoot();
         }
 
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition) -ReferencePoint.position;
+        //Mouse pos initializing
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition) - ReferencePoint.position;
     }   
 
     void FixedUpdate()
     {
+        //Aim follows the cursor
         float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0f, 0f, angle);
     }
 
     void GoDirection()
     {
-        rb.AddForce(-Firepoint.right * dashSpeed, ForceMode2D.Impulse);    }
+        //Character goes the opposite direction of where clicked
+        rb.AddForce(-Firepoint.right * dashSpeed, ForceMode2D.Impulse);
+    }
 
     void Shoot()
     {
+        //Launchs amount of i bullets with spreading
         for(int i = 0; i < bulletCount; i++)
         {
-            GameObject bullet = Instantiate(particle, Firepoint.position, Firepoint.rotation);
+            GameObject bullet = Instantiate(bulletPrefab, Firepoint.position, Firepoint.rotation);
             Rigidbody2D BulletRB = bullet.GetComponent<Rigidbody2D>();
             Vector2 dir = Firepoint.transform.rotation * Vector2.right;
             Vector2 pdir = Vector2.Perpendicular(dir) * Random.Range(-spread, spread);
