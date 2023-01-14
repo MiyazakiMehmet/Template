931c4a2b25d71016cb68e396d0fe2482881e8991
Mehmet <mehmetkoray_@hotmail.com>
2023-01-14 20:11:30 +0300

    Something added

--- My project (2)/Assets/Scripts/AmmoBarScript.cs
+++ My project (2)/Assets/Scripts/AmmoBarScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,15 +8,32 @@ public class AmmoBarScript : MonoBehaviour
 {
     public Slider AmmoSlider;
 
-
+    [SerializeField] private WeaponSwitch WeaponHolder;
+ 
     void Update()
     {
         AmmoSlider = GetComponent<Slider>();
+
     }
 
     public void SetCurrentAmmo(int currentAmmo, int maxAmmo)
     {
         AmmoSlider.maxValue = maxAmmo;
         AmmoSlider.value = currentAmmo;
+    }
+
+    public void CheckCurrentWeapon()
+    {       
+        foreach(Transform weapon in WeaponHolder.transform)
+        {
+            if(weapon.tag == "Shotgun")
+            {
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                gameObject.SetActive(true);
+            }
+        }
     }
 }
---
+++ My project (2)/Assets/Scripts/WeaponSwitch.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class WeaponSwitch : MonoBehaviour
+{
+    private int selectedWeaponIndex = 0;
+
+    void Start()
+    {
+        SwitchWeapon();    
+    }
+
+    void Update()
+    {
+        int previousWeaponIndex = selectedWeaponIndex;
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            selectedWeaponIndex = 0;
+        }
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            selectedWeaponIndex = 1;
+        }
+
+        if(previousWeaponIndex != selectedWeaponIndex)
+        {
+            SwitchWeapon();
+        }
+    }
+
+    void SwitchWeapon()
+    {
+        int i = 0;
+        foreach(Transform weapon in transform)
+        {
+            if(selectedWeaponIndex == i)
+            {
+                weapon.gameObject.SetActive(true);
+            }
+            else
+            {
+                weapon.gameObject.SetActive(false);
+            }
+            i++;
+        }
+    }
+}
